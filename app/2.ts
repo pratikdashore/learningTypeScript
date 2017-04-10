8aa7fa09e88fb7bc0d003633beb2229b3cf1ccc8
Pratik Dashore <pratikdashore@hotmail.com>
2017-04-10 14:25:34 +0530

    changes for optional param

--- app/2.ts
+++ app/2.ts
@@ -5,7 +5,7 @@
   public id: number;
   private _height:number;
 
-  set height(value:number){
+  set height(value:number) {
     this._height = value;
   }
 
@@ -13,8 +13,13 @@
     return this._height;
   }
   
-  constructor(lName : string, fName : string){
+  constructor(lName : string, fName : string, height? : number) {
     this.lName = lName;
     this.fName = fName;
+    this.height = height;
   }
 }
+
+
+var student1 = new student('Dashore','Pratik', 10);
+console.log(student1.height);
